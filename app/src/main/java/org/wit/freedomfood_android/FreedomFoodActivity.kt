4a79b05799d08ef6cd9c8a0befd55b140ffbe1b8
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2021-12-13 17:23:15 +0000

    v2.2.0 - Adding Event Handling

--- app/src/main/java/org/wit/freedomfood_android/FreedomFoodActivity.kt
+++ app/src/main/java/org/wit/freedomfood_android/FreedomFoodActivity.kt
@@ -2,16 +2,36 @@ package org.wit.freedomfood_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.freedomfood_android.databinding.ActivityFreedomfoodBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class FreedomFoodActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFreedomfoodBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_freedomfood)
+
+        binding = ActivityFreedomfoodBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
 
         i("FreedomFood Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val placemarkTitle = binding.freedomfoodTitle.text.toString()
+            if (placemarkTitle.isNotEmpty()) {
+                i("add Button Pressed: $placemarkTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+
     }
 }
