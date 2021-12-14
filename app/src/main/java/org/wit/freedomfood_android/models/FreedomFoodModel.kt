ef9728efca8f800d61d22fd21d8826877e4db551
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2021-12-14 13:03:36 +0000

    v4.2.0 - Making FreedomFoodModel parcelable

--- app/src/main/java/org/wit/freedomfood_android/models/FreedomFoodModel.kt
+++ app/src/main/java/org/wit/freedomfood_android/models/FreedomFoodModel.kt
@@ -1,4 +1,8 @@
 package org.wit.freedomfood_android.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class FreedomFoodModel(var title: String = "",
-                            var description: String = "")
+                            var description: String = "") : Parcelable
