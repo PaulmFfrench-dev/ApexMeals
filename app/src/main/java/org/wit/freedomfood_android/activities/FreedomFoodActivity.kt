bbc08433ceb6895e72b75771cb84e903314964a4
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2021-12-14 15:53:45 +0000

    v6.0.0 - Adding Location Resources

--- app/src/main/java/org/wit/freedomfood_android/activities/FreedomFoodActivity.kt
+++ app/src/main/java/org/wit/freedomfood_android/activities/FreedomFoodActivity.kt
@@ -69,6 +69,10 @@ class FreedomFoodActivity : AppCompatActivity() {
             showImagePicker(imageIntentLauncher)
         }
 
+        binding.freedomfoodLocation.setOnClickListener {
+            i ("Set Location Pressed")
+        }
+
         registerImagePickerCallback()
     }
 
