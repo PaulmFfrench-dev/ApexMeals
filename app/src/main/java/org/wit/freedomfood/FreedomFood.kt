956ac09c6b7f8752f5cb813bbbde8038e0575eb6
PaulmFfrench-dev <paulmffrench@gmail.com>
2022-03-23 12:02:57 +0000

    v8.1.0 - Making a Donation

--- app/src/main/java/org/wit/freedomfood/FreedomFood.kt
+++ app/src/main/java/org/wit/freedomfood/FreedomFood.kt
@@ -2,10 +2,40 @@ package org.wit.freedomfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.wit.freedomfood.databinding.ActivityFreedomfoodBinding
 
 class FreedomFood : AppCompatActivity() {
+
+    private lateinit var donateLayout : ActivityFreedomfoodBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_freedomfood)
+        donateLayout = ActivityFreedomfoodBinding.inflate(layoutInflater)
+        setContentView(donateLayout.root)
+
+        donateLayout.progressBar.max = 10000
+
+        donateLayout.amountPicker.minValue = 1
+        donateLayout.amountPicker.maxValue = 1000
+
+        donateLayout.amountPicker.setOnValueChangedListener { _, _, newVal ->
+            //Display the newly selected number to paymentAmount
+            donateLayout.paymentAmount.setText("$newVal")
+        }
+
+        var totalDonated = 0
+
+        donateLayout.donateButton.setOnClickListener {
+            val amount = if (donateLayout.paymentAmount.text.isNotEmpty())
+                donateLayout.paymentAmount.text.toString().toInt() else donateLayout.amountPicker.value
+            if(totalDonated >= donateLayout.progressBar.max)
+                Toast.makeText(applicationContext,"Donate Amount Exceeded!",Toast.LENGTH_LONG).show()
+            else {
+                totalDonated += amount
+                donateLayout.totalSoFar.text = "$$totalDonated"
+                donateLayout.progressBar.progress = totalDonated
+            }
+        }
     }
 }
