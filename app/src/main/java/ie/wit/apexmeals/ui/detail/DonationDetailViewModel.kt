3160270249cd8fe3997bfe32ce18e6fe8ad515e3
PaulmFfrench-dev <paulmffrench@gmail.com>
2022-03-23 21:20:38 +0000

    v10.6.0 - Adding DonationDetailViewModel

--- app/src/main/java/ie/wit/apexmeals/ui/detail/DonationDetailViewModel.kt
+++ app/src/main/java/ie/wit/apexmeals/ui/detail/DonationDetailViewModel.kt
@@ -1,7 +1,18 @@
 package ie.wit.apexmeals.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ie.wit.apexmeals.models.ApexMealsManager
+import ie.wit.apexmeals.models.ApexMealsModel
 
 class DonationDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val donation = MutableLiveData<ApexMealsModel>()
+
+    val observableDonation: LiveData<ApexMealsModel>
+        get() = donation
+
+    fun getDonation(id: Long) {
+        donation.value = ApexMealsManager.findById(id)
+    }
 }
